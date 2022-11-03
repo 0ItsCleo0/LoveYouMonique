434113791b34b28f86a8b6bbf341fd837a635ee0
Cleo Valentine ♡ <82450945+0ItsCleo0@users.noreply.github.com>
2022-11-03 15:34:15 -0500

    Updated Paragraphs

--- Monique/app.js
+++ Monique/app.js
@@ -25,16 +25,15 @@ function pageTrans(direction) {
 
         pageOpen
             .fromTo(date, {zIndex: -1}, {zIndex: 2, duration: 0.1})
-            .fromTo('#timeline', {height: 'fit-content'}, {height: '100vh', duration: 0})
             .fromTo(date, {backgroundColor: '#1f1f1f00'}, {backgroundColor: 'rgba(255, 255, 255)', duration: 0.1})
             .fromTo(date + ' .date', {color: '#1f1f1f00'}, {color: '#1f1f1f', duration: 0.5})
             .fromTo(rule, {width: '0'}, {width: '80%', duration: 1, delay: 0.5})
-            .fromTo(date + ' p', {margin: '-15em 0', height: 0}, {margin: '0 0', height: '6em', duration: 0.6, delay: 0.1})
+            .fromTo(date + ' p', {margin: '-15em 0', height: 0}, {margin: '0 0', height: '7em', duration: 0.6, delay: 0.1})
             .fromTo(date + ' p', {color: '#1f1f1f00'}, {color: '#1f1f1f', duration: 0.5, delay: 0.1})
             .fromTo(date + ' ul h1', {color: '#1f1f1f00'}, {color: '#1f1f1f', duration: 0.5}, '-=0.2')
             .fromTo(rule2, {backgroundColor: 'transparent'}, {backgroundColor: '#1f1f1f', duration: 0.5}, '-=0.2')
             .fromTo(date + ' ul', {color: '#1f1f1f00'}, {color: '#1f1f1f', duration: 0.5}, '-=0.2')
-            .fromTo(date + ' svg', {stroke: '#1f1f1f00'}, {stroke: '#1f1f1f', duration: 1})
+            .fromTo(date + ' svg', {stroke: '#1f1f1f00'}, {stroke: '#1f1f1f', duration: 1}, '-=1')
     } else {
         pageClose.play()
 
@@ -43,12 +42,11 @@ function pageTrans(direction) {
             .fromTo(rule2, {backgroundColor: '#1f1f1f'}, {backgroundColor: 'transparent', duration: 0.2})
             .fromTo(date + ' ul h1', {color: '#1f1f1f'}, {color: '#1f1f1f00', duration: 0.2})
             .fromTo(date + ' p', {color: '#1f1f1f'}, {color: '#1f1f1f00', duration: 0.2})
-            .fromTo(date + ' p', {margin: '0 0', height: '6em'}, {margin: '-15em 0', height: 0, duration: 0.5})
+            .fromTo(date + ' p', {margin: '0 0', height: '7em'}, {margin: '-15em 0', height: 0, duration: 0.5})
             .fromTo(rule, {width: '80%'}, {width: '0', duration: 0.2})
             .fromTo(date + ' .date', {color: '#1f1f1f'}, {color: '#1f1f1f00', duration: 0.2})
             .fromTo(date, {backgroundColor: 'rgba(255, 255, 255)'}, {backgroundColor: '#1f1f1f00', duration: 0.2})
-            .fromTo('#timeline', {height: '100vh'}, {height: 'fit-content', duration: 1})
-            .fromTo(date, {zIndex: 2}, {zIndex: -10, duration: 1}, '-=0.8')
+            .fromTo(date, {zIndex: 2}, {zIndex: -10, duration: 1})
     }
 }
 
